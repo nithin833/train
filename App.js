c6dc0649797ff435b88f1a698fc7834afb9c443e
nithin833 <147524590+nithin833@users.noreply.github.com>
2023-10-10 20:38:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import react from 'react'
+const App = () => {
+  return (
+    <div>
+      <h1>This is a web page</h1>
+      <p>nothing to display here</p>
+    </div>
+  )
+}
+export default App;
+ 
